19d1b4e252346b2d73fe3014160ba2ef427c8a0e
Barathvajk <Barathvajk>
2025-07-29 14:56:58 +0530

    UTIL ADDED

--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/shop',(req,res,next)=>
 {
- res.sendFile(path.join(__dirname, '../','views', 'shop.html')); 
+ res.sendFile(path.join(rootDir,'views', 'shop.html')); 
 });
 
 module.exports = router;
